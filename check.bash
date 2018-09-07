fae91a4700114283b7f6eea284c452accbc5088e
thatano <adad0405@gmail.com>
2018-09-07 11:10:19 +0900

    add check.bash2

---
+++ check.bash
@@ -0,0 +1,1 @@
+docker-compose exec pwscup2018sample java -jar /pwscup2018sample/checker/checker.jar /pwscup2018sample/drill/data/T.csv /pwscup2018sample/drill/data/A.csv
--- eval.bash
+++ eval.bash
@@ -1,1 +1,1 @@
-docker-compose exec pwscup2018sample /bin/bash /pwscup2018-sample/drill/command.sh
+docker-compose exec pwscup2018sample /bin/bash /pwscup2018sample/drill/command.sh
