655ee6992f6f8ce50ed809701ceb065117538327
thatano <adad0405@gmail.com>
2018-09-13 17:29:55 +0900

    add check-F

---
+++ check-F.bash
@@ -0,0 +1,1 @@
+docker-compose exec pwscup2018sample /bin/bash /pwscup2018sample/check-f1/check-f1.sh /pwscup2018sample/drill/data/T.csv /pwscup2018sample/drill/data/A.csv /pwscup2018sample/drill/data/F.csv
